d4180fd79e70fea875228a2f34958edcf5661437
sbalayan1 <balayans2014@yahoo.com>
2022-04-14 06:04:17 -0700

    WIP

---
+++ js/challenge.js
@@ -0,0 +1,18 @@
+let counter = document.querySelector('#counter')
+let minus = document.querySelector('#minus')
+let plus = document.querySelector('#plus')
+let heart = document.querySelector('#heart')
+let pause = document.querySelector('#pause')
+let commentForm = document.querySelector('#comment-form')
+let commentInput = document.querySelector('#comment-input')
+let likesList = document.querySelector('.likes')
+
+let counterEvent = () => {
+    setInterval(() => {
+        counter.textContent = parseInt(counter.textContent) + 1
+    }, 1000)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    counterEvent()
+})
