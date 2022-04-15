2ef9394ed9280c0e9c474b1d6692209ba689a9c5
sbalayan1 <balayans2014@yahoo.com>
2022-04-14 20:26:12 -0700

    commit tree a2157b35234073f806c5718b0207e4d2baaef891

--- js/challenge.js
+++ js/challenge.js
@@ -2,17 +2,48 @@ let counter = document.querySelector('#counter')
 let minus = document.querySelector('#minus')
 let plus = document.querySelector('#plus')
 let heart = document.querySelector('#heart')
-let pause = document.querySelector('#pause')
+let intervalButton = document.querySelector('#pause')
 let commentForm = document.querySelector('#comment-form')
 let commentInput = document.querySelector('#comment-input')
 let likesList = document.querySelector('.likes')
+let counterEvent = setInterval(() => {counter.textContent = parseInt(counter.textContent) + 1}, 1000)
 
-let counterEvent = () => {
-    setInterval(() => {
-        counter.textContent = parseInt(counter.textContent) + 1
-    }, 1000)
+
+
+let controlInterval = () => {
+    if (intervalButton.textContent === ' pause ') {
+        console.log('paused')
+        clearInterval(counterEvent)
+        intervalButton.textContent = ' resume '
+        removeListeners()
+    } else if (intervalButton.textContent === ' resume ') {
+        console.log('resumed')
+        counterEvent = setInterval(() => {counter.textContent = parseInt(counter.textContent) + 1}, 1000)
+        intervalButton.textContent = ' pause '
+        addListeners()
+    }
+}
+
+let addListeners = () => {
+    minus.addEventListener('click', () => {counter.textContent = parseInt(counter.textContent) - 1})
+    plus.addEventListener('click', () => {counter.textContent = parseInt(counter.textContent) + 1})
+    heart.addEventListener('click', () => {})
+    minus.disabled = false
+    plus.disabled = false
+    heart.disabled = false
+}
+
+let removeListeners = () => {
+    minus.removeEventListener('click', () => {})
+    plus.removeEventListener('click', () => {})
+    heart.removeEventListener('click', () => {})
+    minus.disabled = true
+    plus.disabled = true
+    heart.disabled = true
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    counterEvent()
+    counterEvent
+    intervalButton.addEventListener('click', controlInterval)
+    addListeners()
 })
